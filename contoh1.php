dba14e224a9e5defa3af0c96c84f39bad4803b0a
wp2prasetyoagustian <90603658+wp2prasetyoagustian@users.noreply.github.com>
2021-09-13 16:53:14 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "perkenalkan saya prasetyo agustian<br>";
+    echo "Saya mahasiswa kelas 19.3A.05";
+    }
+}
